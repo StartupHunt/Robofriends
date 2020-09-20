22d90c31c5f83ac99b170918e0c00f2e1c969f2c
Alejandro <alejandro@cafedelossentidos.com>
2020-09-19 21:03:27 -0500

    Updates

---
+++ precache-manifest.d92e848f57f66e886697eaf170e45668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82d3bde8ecb65088c7cb5f8fc2e0e98",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b0e553a3259720d30e86",
+    "url": "/Robofriends/static/css/main.064ea7a6.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e553a3259720d30e86",
+    "url": "/Robofriends/static/js/main.82b98ec6.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  }
+]);
